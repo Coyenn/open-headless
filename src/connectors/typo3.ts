555d18e624dbd6e5c97b77aeb4f7d63133f9534d
Tim Ritter <timrittermail@gmail.com>
2023-08-09 14:43:11 +0200

    initial commit

---
+++ src/connectors/typo3.ts
@@ -0,0 +1,55 @@
+/**
+* The BoxElement is a content element that can be rendered.
+*/
+type BoxElement = ContentElement;
+
+/**
+* A loop element can contain one or more content elements.
+* (A list is a loop element, for example)
+*/
+interface LoopElement extends IdentifiableObject {
+  name: string;
+  type: string;
+  content?: BoxElement[];
+
+  [key: string]: any;
+}
+
+/**
+* A grid element is a container for other elements.
+* It can contain other grid elements or content elements.
+*/
+interface GridElement extends IdentifiableObject {
+  name: string;
+  type: string;
+  content?: BoxElement | GridElement;
+
+  [key: string]: any;
+}
+
+const typo3Connector: CmsConnector = {
+  requestPage: async (path: string, cmsPath: string) => {
+    const requestUrl = new URL(path, cmsPath);
+    const response = await fetch(requestUrl.toString());
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch page from ${requestUrl.toString()}`);
+    }
+
+    const cmsResponse = await response.json() as CmsResponse;
+
+    if (!cmsResponse || !('id' in cmsResponse)) {
+      throw new Error(`Invalid response from ${requestUrl.toString()}`);
+    }
+
+    if ('target' in cmsResponse) {
+      return cmsResponse as Redirect;
+    } else {
+      return cmsResponse as Page;
+    }
+  }
+};
+
+
+export default typo3Connector;
+export { GridElement, LoopElement, BoxElement };
---
+++ src/core.ts
@@ -0,0 +1,51 @@
+interface ProjectConfiguration {
+  name: string;
+  cmsConnector: CmsConnector;
+  cmsPath: string;
+
+  [key: string]: any;
+}
+
+interface IdentifiableObject {
+  id: string;
+}
+
+interface PageMeta {
+  title?: string;
+  description?: string;
+  keywords?: string[];
+  image?: string;
+  url?: string;
+
+  [key: string]: any;
+}
+
+interface ContentElement extends IdentifiableObject {
+  name: string;
+  type: string;
+  content?: { [key: string]: any };
+
+  [key: string]: any;
+}
+
+interface Page extends IdentifiableObject {
+  meta?: PageMeta;
+  content?: ContentElement[];
+
+  [key: string]: any;
+}
+
+interface Redirect extends IdentifiableObject {
+  statusCode?: number;
+  target: string;
+}
+
+type CmsResponse = Page | Redirect;
+
+interface CmsConnector {
+  requestPage(path?: string, cmsPath?: string): Promise<Page | Redirect>;
+
+  [key: string]: any;
+}
+
+export { ProjectConfiguration, CmsConnector, CmsResponse, Page, Redirect, PageMeta, ContentElement, IdentifiableObject };
